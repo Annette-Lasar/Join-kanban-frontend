91e1d7b7067faba34b97dee1faa43b29ad46cb02
Annette Lasar <a_lasar@web.de>
2026-03-26 07:45:28 +0100

    Switch back to BASE_URL for production

--- src/app/shared/constants/global-constants.data.ts
+++ src/app/shared/constants/global-constants.data.ts
@@ -1,7 +1,7 @@
 // for development only!
-export const BASE_URL = 'http://127.0.0.1:8000/api';
+// export const BASE_URL = 'http://127.0.0.1:8000/api';
 // for production!
-// export const BASE_URL = 'https://api.join.annette-lasar.de/api';
+export const BASE_URL = 'https://api.join.annette-lasar.de/api';
 
 export const maxTextLength = 6;
 
